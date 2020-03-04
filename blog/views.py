46b6bd2bc11ce969e25996ea3855ccbf3bb55293
Ais53 <ais53ez53sa2@gmail.com>
2020-03-04 12:26:19 +0100

    Show posts on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    me = User.objects.all()[0]
+    posts = Post.objects.filter(author=me)
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
