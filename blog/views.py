aca61c5a61c002b9ef60999684e657a37ee5eb20
Ais53 <ais53ez53sa2@gmail.com>
2020-03-05 10:47:11 +0100

    Finally

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.contrib.auth.models import User
 from .models import Post
@@ -8,4 +8,9 @@ def post_list(request):
     me = User.objects.all()[0]
     posts = Post.objects.filter(author=me)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
